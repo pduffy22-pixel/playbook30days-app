ddba6ad7c52a1191bb127cd02507ed126567cc02
pduffy22-pixel <pduffy22@gmail.com>
2026-09-02 16:12:41 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -173,7 +173,6 @@ export default defineConfig(({ command, isPreview }) => ({
             preset: "static",
             prerender: { enabled: false },
             // Auto-registers server/middleware/* ...
-            serverDir: "./server",
           }),
         ]
       : []),
